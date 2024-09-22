5a04c4edafe261ea0c3fa9cae30e66c7a8760e56
Jezer Mejía <amyuki4@icloud.com>
2024-09-21 23:48:57 -0600

    Fix CMakeLists

--- common/util/os.h
+++ common/util/os.h
@@ -6,6 +6,12 @@
 size_t get_peak_rss();
 void setup_cpu_info();
 
+enum CpuArch {
+  cpu_arch_unknown = 0,
+  cpu_arch_x86_64 = 1,
+  cpu_arch_arm64 = 2,
+};
+
 struct CpuInfo {
   bool initialized = false;
   bool has_avx = false;
